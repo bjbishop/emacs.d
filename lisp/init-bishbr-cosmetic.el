a94305831b4039264655f65a655fc8102f6ee1ab
Bryan Bishop <bryan.bishop@pearson.com>
2024-07-12 17:47:28 -0500

    Window movement and colors

--- lisp/init-bishbr-cosmetic.el
+++ lisp/init-bishbr-cosmetic.el
@@ -5,54 +5,76 @@
 
 ;;; Code:
 
-(use-package catppuccin-theme
-  :ensure t
+
+
+(use-package windmove
+  :bind (
+         ("A-n" . windmove-left)
+         ("A-h" . windmove-right)
+         ("A-i" . windmove-down)
+         ("A-r" . windmove-up)
+         ("s-n" . windmove-left)
+         ("s-h" . windmove-right)
+         ("s-i" . windmove-down)
+         ("s-r" . windmove-up)
+         ("C-A-n" . 'shrink-window-horizontally)
+         ("C-A-h" . 'enlarge-window-horizontally)
+         ("C-A-r" . 'enlarge-window)
+         ("C-A-i" . 'shrink-window)
+         ("C-A-+" . 'balance-windows)
+         )
+  :custom
+  (windmove-wrap-around nil)
   )
 
+
+(use-package doom-themes)
+(use-package catppuccin-theme)
+
 ;; 'frappe, 'latte, 'macchiato, 'mocha
-(setq catppuccin-flavor 'latte)
-(catppuccin-reload)
 
 ;; Don't zoom text with the mouse or trackpad
 (global-set-key (kbd "<pinch>") 'ignore)
 (global-set-key (kbd "<C-wheel-up>") 'ignore)
 (global-set-key (kbd "<C-wheel-down>") 'ignore)
 
-;;doom-oksolar-light) ;;kaolin-breeze) ;;flexoki-themes-light) ;; solarized-selenized-light)
+(defun my/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light
+     (setq catppuccin-flavor 'latte)
+     (catppuccin-reload)
 
-;; (defun my/apply-theme (appearance)
-;;   "Load theme, taking current system APPEARANCE into consideration."
-;;   (mapc #'disable-theme custom-enabled-themes)
-;;   (pcase appearance
-;;     ('light
+     (add-to-list 'default-frame-alist '(ns-appearance . light))
+     (add-to-list 'frameset-filter-alist '(ns-transparent-titlebar . :never))
+     (modify-all-frames-parameters '(
+                                     (ns-appearance . light)
+                                     ))
+     )
+    ('dark
+     (load-theme 'deeper-blue)
 
-;;      ;; (load-theme 'kaolin-mono-light)
+     (add-to-list 'default-frame-alist '(ns-appearance . dark))
+     (add-to-list 'frameset-filter-alist '(ns-transparent-titlebar . :never))
+     (modify-all-frames-parameters '(
+                                     (ns-appearance . dark)
+                                     ))
+     )))
 
-;;      (setq catppuccin-flavor 'latte)
-;;      (catppuccin-reload)
-
-;;      (add-to-list 'default-frame-alist '(ns-appearance . light))
-;;      (add-to-list 'frameset-filter-alist '(ns-transparent-titlebar . :never))
-;;      (modify-all-frames-parameters '(
-;;                                      (ns-appearance . light)
-;;                                      ))
-;;      )
-;;     ('dark
-
-;;      ;; (load-theme 'doom-oceanic-next)
-
-;;      (setq catppuccin-flavor 'frappe)
-;;      (catppuccin-reload)
-
-;;      (add-to-list 'default-frame-alist '(ns-appearance . dark))
-;;      (add-to-list 'frameset-filter-alist '(ns-transparent-titlebar . :never))
-;;      (modify-all-frames-parameters '(
-;;                                      (ns-appearance . dark)
-;;                                      ))
-;;      )))
-
-;; (add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
+(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
 ;; (my/apply-theme 'light)
+
+(defun light ()
+  "Activate a light color theme."
+  (interactive)
+  (my/apply-theme 'light))
+
+(defun dark ()
+  "Activate a dark color theme."
+  (interactive)
+  (my/apply-theme 'dark)
+  )
 
 ;; https://karthinks.com/software/batteries-included-with-emacs/
 (defun pulse-line (&rest _)
